dcba10bd150f7c51ec457b905747096580be9bb3
Shawn Khandia <shawnk04@email.unc.edu>
2022-04-21 18:24:54 -0400

    Require express

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ const fs = require('fs')
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const args = require('minimist')(process.argv.slice(2))
 
+// Require Express.js
+const express = require('express')
+const app = express()
+
 // Define allowed argument name 'port'.
 args['port']
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 const port = args.port || process.env.PORT || 3000
+
